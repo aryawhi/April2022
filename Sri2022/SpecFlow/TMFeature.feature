6bcf8294626c348cf2906699410a09b02efad7a3
Aryawhi <srilaxmikamalapur@gmail.com>
2022-05-05 22:09:53 +1200

    load test implementation

---
+++ Sri2022/SpecFlow/TMFeature.feature
@@ -0,0 +1,13 @@
+﻿Feature: TMFeature
+
+As a turn up portal admin 
+I would like to create, edit and delete time and material records
+so that I can manage employees time and materials successfully
+
+@regression
+Scenario: create time and material record with valid details
+	Given I logged in to turn up portal successfully
+	When I navigate to Time and Material page
+	When I create a new time and material record
+	Then the record should be created successfully
+
---
+++ Sri2022/TMFeatureStepDefinitions.cs
@@ -0,0 +1,33 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace Sri2022
+{
+    [Binding]
+    public class TMFeatureStepDefinitions
+    {
+        [Given(@"I logged in to turn up portal successfully")]
+        public void GivenILoggedInToTurnUpPortalSuccessfully()
+        {
+            
+        }
+
+        [When(@"I navigate to Time and Material page")]
+        public void WhenINavigateToTimeAndMaterialPage()
+        {
+            
+        }
+
+        [When(@"I create a new time and material record")]
+        public void WhenICreateANewTimeAndMaterialRecord()
+        {
+           
+        }
+
+        [Then(@"the record should be created successfully")]
+        public void ThenTheRecordShouldBeCreatedSuccessfully()
+        {
+            
+        }
+    }
+}
